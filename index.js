83e2c2b43815333071d62ac4269c33b4b69ff156
minhquocece <lonelyblue.hb@gmail.com>
2019-03-06 23:27:35 +0700

    fixed listen(PORT)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const	app = express();
+const app = express();
 
 app.get('/', (req, res) => {
 	res.send({ hi: 'there' });
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000);
+app.listen(PORT);
 
 
