c3a7b957dfa9c084f059f5c30ae15271af62a798
minhquocece <lonelyblue.hb@gmail.com>
2019-03-10 17:45:06 +0700

    fixed mongoose connection

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,6 @@
 module.exports = {
 	googleClientID: process.env.GOOGLE_CLIENT_ID,
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-	mongoURI: 'mongodb+srv://wedding-card-prod:wedding-card-pro@wedding-card-prod-q2ppm.mongodb.net/test?retryWrites=true',
+	mongoURI: process.env.MONGO_URI,
 	cookieKey: process.env.COOKIE_KEY
 };
