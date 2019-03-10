9f75a6d59a1db0753cae6d292267b45d60cf95a4
minhquocece <lonelyblue.hb@gmail.com>
2019-03-10 16:29:23 +0700

    finished auth flow

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,7 @@
-module.exports = {
-	googleClientID: '425480936986-j1p7vgue53du07os1c4k0i9f4e1r3e3c.apps.googleusercontent.com',
-	gooClientSecret: 'D28OguYOw6WkwLhNz0rFSBeX',
-};
+//key.js - figure out what set of credentials to return
+if (process.env.NODE_ENV === 'production') {
+	module.exports = require('./prod');	
+} else {
+	// we are in development - return the dev keys!!!
+	module.exports = require('./dev');	
+}
