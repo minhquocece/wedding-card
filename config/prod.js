a596e073b03fe86bede0f2dcca01e828b4753077
minhquocece <lonelyblue.hb@gmail.com>
2019-03-10 17:09:51 +0700

    test mongo

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,6 @@
 module.exports = {
 	googleClientID: process.env.GOOGLE_CLIENT_ID,
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-	mongoURI: process.env.MONGO_URI,
+	mongoURI: 'mongodb+srv://wedding-card-prod:wedding-card-pro@wedding-card-prod-q2ppm.mongodb.net/test?retryWrites=true',
 	cookieKey: process.env.COOKIE_KEY
 };
