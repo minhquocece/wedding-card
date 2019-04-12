b2541fa0e3872fac02e4ea24f481e242cee941c4
minhquocece <lonelyblue.hb@gmail.com>
2019-04-12 15:43:17 +0700

    added feedback feature

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require("lodash");
+const Path = require("path-parser");
+const { URL } = require("url");
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
@@ -12,8 +15,6 @@ module.exports = app => {
   });
 
   app.post("/api/surveys/webhooks", (req, res) => {
-    console.log("here");
-    console.log(req.body);
     res.send({});
   });
 
