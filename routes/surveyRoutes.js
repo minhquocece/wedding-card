3fecfae5e51ed1658e11a9b8d97fe6ad22c2b5b1
minhquocece <lonelyblue.hb@gmail.com>
2019-04-19 18:47:23 +0700

    get feedback

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,5 +1,5 @@
 const _ = require("lodash");
-const Path = require("path-parser");
+const { Path } = require("path-parser");
 const { URL } = require("url");
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
@@ -15,6 +15,32 @@ module.exports = app => {
   });
 
   app.post("/api/surveys/webhooks", (req, res) => {
+    const p = new Path("/api/surveys/:surveyId/:choice");
+    _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice };
+        }
+      })
+      .compact()
+      .uniqBy("email", "surveyId")
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false }
+            }
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { "recipients.$.responded": true }
+          }
+        ).exec();
+      })
+      .value();
+
     res.send({});
   });
 
